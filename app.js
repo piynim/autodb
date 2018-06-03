b896ebc06e7e642517d7be3bededb7ec563a808c
piynim <piyushnimje190@gmail.com>
2018-06-04 00:32:27 +0530

    backend ready

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var http = require('http');
-
+var routes = require('./routes/userRoutes');
 var app = express();
+const port = 9000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', function (req, res) {
+app.get('/', function handler(req, res) {
     res.send("Welcome to Express App");
+});
+const mongoServer = `mongodb://localhost:27017/User`;
+
+
+
+mongoose.connect(mongoServer, function(err, data){
+    if(err){
+        console.log(err);
+    }
+    else {
+        console.log('Database connected');
+    }
 })
 
+app.use('/user', routes);
 http.createServer(app)
-    .listen(3000, function (err) {
+    .listen(port, function (err) {
         if (err) {
             console.log("Couldn't start app server");
             return;
---
+++ routes/service.js
@@ -0,0 +1,11 @@
+
+
+function timeDifferenceCalc(){
+   console.log('here'); 
+    var date1 = new Date();
+    var date2 = new Date();
+    var result = toString(date2 - date1);
+    console.log(result);
+}
+
+timeDifferenceCalc();
